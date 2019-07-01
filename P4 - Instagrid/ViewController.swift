ccf4128900ff8a31f8c7db4c1069a72af092dc4d
JBDeslandes <jbdeslandes@hotmail.fr>
2019-07-01 20:08:18 +0200

    Main.storyboard added for Portrait & Landscape mods

--- P4 - Instagrid/ViewController.swift
+++ P4 - Instagrid/ViewController.swift
@@ -13,8 +13,48 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        grid = .layout2
     }
-
-
+    
+    // MARK: - Properties
+    
+    var grid: Grid = .layout2 {
+        didSet {
+            displayDidChange()
+        }
+    }
+    
+    // MARK: - Methods
+    
+    func displayDidChange() {
+        for i in 0..<contentViews.count {
+            let value = grid.display[i]
+            contentViews[i].isHidden = value
+        }
+    }
+    
+    
+    // MARK: - Outlets
+    
+    @IBOutlet var contentViews: [UIView]!
+    @IBOutlet var patternButtons: [UIButton]!
+    
+    // MARK: - Actions
+    
+    @IBAction func patternButtonTapped(_ sender: UIButton) {
+        patternButtons.forEach { $0.isSelected = false }
+        sender.isSelected = true
+        switch sender.tag {
+        case 0:
+            grid = .layout1
+        case 1:
+            grid = .layout2
+        case 2:
+            grid = .layout3
+        default:
+            break
+        }
+    }
+    
 }
 
